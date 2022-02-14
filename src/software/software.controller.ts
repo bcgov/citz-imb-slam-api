0f2839dd232340ece992b686aa8827b0df771687
Scott Toews <scott.toews@gov.bc.ca>
2022-02-14 14:40:18 -0800

    remmed out db connections

--- src/software/software.controller.ts
+++ src/software/software.controller.ts
@@ -3,12 +3,21 @@ import { Crud, CrudController } from '@nestjsx/crud';
 import { SoftwareTitle } from './software.entity';
 import { SoftwareService } from './software.service';
 
-@Crud({
-  model: {
-    type: SoftwareTitle,
-  },
-})
+// @Crud({
+//   model: {
+//     type: SoftwareTitle,
+//   },
+// })
+// @Controller('software')
+// export class SoftwareController implements CrudController<SoftwareTitle> {
+//   constructor(public service: SoftwareService) {}
+// }
+
+//mock
 @Controller('software')
-export class SoftwareController implements CrudController<SoftwareTitle> {
-  constructor(public service: SoftwareService) {}
+export class SoftwareController {
+  @Get()
+  getSoftware() {
+    return 'the software';
+  }
 }
