8e793f0e2e1a419bcf16965d580a4a699c1dc3ca
Scott Toews <scott.toews@gov.bc.ca>
2022-02-24 12:55:53 -0800

    added default values to db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SoftwareModule } from './software/software.module';
 import { ConfigModule } from '@nestjs/config';
 
+const env = {
+  host: process.env.HOST || 'localhost',
+  port: 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'snowball',
+  database: process.env.POSTGRES_DATABASE || 'slam-db',
+  synchronize: process.env.POSTGRES_SYNCHRONIZE === 'true' || false,
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     SoftwareModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.HOST,
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      host: env.host,
+      port: env.port,
+      username: env.username,
+      password: env.password,
+      database: env.database,
       entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: process.env.POSTGRES_SYNCHRONIZE === 'true',
+      synchronize: env.synchronize,
     }),
   ],
   controllers: [],
