616e55cd015ed40164a7d117c3341c57c58c35ac
Scott Toews <scott.toews@gov.bc.ca>
2022-06-27 11:41:31 -0700

    ran format lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,6 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 
-const env = {
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: process.env.POSTGRES_SYNCHRONIZE === 'true' || false,
-};
 @Module({
     imports: [
         AssignedLicenseModule,
