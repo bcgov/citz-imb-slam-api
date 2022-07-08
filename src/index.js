a80e409f9a63b3aa721a5c3b8b9c0ef170e17f47
Scott Toews <scott.toews@gov.bc.ca>
2022-07-08 12:15:38 -0700

    updated cors to include credentials

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 const PORT = 3001
 
-app.use(cors({ origin: '*' }))
+app.use(cors({ origin: true, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(session({
