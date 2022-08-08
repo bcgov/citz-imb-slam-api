c40cbb7eeed2fa06938d4970f54a90baea2f640b
Matthew Sielecki <msielecki2@gmail.com>
2022-08-08 08:53:25 -0700

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
     constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
+    @Get('/auth/callback/keycloak')
     login(@Body() body): any {
-        return this.authService.login(body.username);
+        return this.authService.login(body.idir_username);
     }
 }
