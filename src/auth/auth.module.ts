ca62a26801a9514eb16680c20af11af3cb203076
dbarkowsky <dylanbarkowsky@gmail.com>
2022-08-11 13:33:38 -0700

    Auth uses licensees. User automatically added.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
+import { LicenseeModule } from 'src/licensee/licensee.module';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
     imports: [
-        UsersModule,
+        LicenseeModule,
         PassportModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET,
